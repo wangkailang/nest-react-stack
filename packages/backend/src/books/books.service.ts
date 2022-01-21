a0d36d4fdc7dec9799a49d02e45c6322cde8ebd6
王开朗 <wangkailang@users.noreply.github.com>
2022-01-21 17:11:33 +0800

    feat: nx monorepo (#2)
    * initial commit
    
    * feat: nx monorepo

--- packages/backend/src/books/books.service.ts
+++ packages/backend/src/books/books.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
 
